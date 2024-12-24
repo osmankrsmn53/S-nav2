2a47291b320dafbf280f316542c674aa4d269d5b
osmankrsmn53 <osmankaraosman256@gmail.com>
2024-12-24 23:02:32 +0300

    Delete aziz sancar.py

--- aziz sancar.py
+++
@@ -1,3 +0,0 @@
-aziz_sancar={"basarısı": "nobel ödülünü aldı","dogum_yeri": "Mardin","çalışması": "DNA onarımının nasıl yapılıcağını buldu","ailesi": "mardinde bir çiftçi","ilgi_duydugu_spor": "Fubol"}
-
-print(aziz_sancar)
